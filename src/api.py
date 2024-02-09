65c9bd758926e1c7ab07e918230c3e32176a06a7
ILKHOM <gaffarovilkhom@inbox.ru>
2024-02-09 12:25:10 +0300

    Added a class for working with the API on hh.ru and Constructor.

--- src/api.py
+++ src/api.py
@@ -10,3 +10,17 @@ class Api(ABC):
     def get_vacancies(self, words):
         """Абстрактный метод для получения списка вакансий по ключевым словам."""
         pass
+
+class HeadHunterAPI(Api):
+    """Класс для работы с API на hh.ru."""
+
+    def __init__(self, count) -> None:
+        """
+        Конструктор с входным параметром количества вакансий.
+        """
+        self.url = 'https://api.hh.ru/vacancies/'
+        self.params = {
+            'per_page': count,
+            'area': 1,
+            'page': 1
+        }
