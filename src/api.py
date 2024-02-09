ef67776d68cd035c7bae07bf8bc4d4609e822c3b
ILKHOM <gaffarovilkhom@inbox.ru>
2024-02-09 12:26:10 +0300

    Added a Method for obtaining a list of vacancies by keywords from the site hh.ru.

--- src/api.py
+++ src/api.py
@@ -11,6 +11,7 @@ class Api(ABC):
         """Абстрактный метод для получения списка вакансий по ключевым словам."""
         pass
 
+
 class HeadHunterAPI(Api):
     """Класс для работы с API на hh.ru."""
 
@@ -24,3 +25,18 @@ class HeadHunterAPI(Api):
             'area': 1,
             'page': 1
         }
+
+    def get_vacancies(self, words):
+        """
+        Метод для получения списка вакансий по ключевым словам с сайта hh.ru.
+
+        Аргументы:
+        words - список ключевых слов для поиска вакансий.
+
+        Возвращает:
+        vacancies - список вакансий, полученных с сайта hh.ru.
+        """
+        self.params['text'] = words
+        r = requests.get(self.url, params=self.params)
+        vacancies = json.loads(r.text)['items']
+        return vacancies
